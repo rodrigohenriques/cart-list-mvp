7076ffe46adbb449c447e84537289bcd8e424a78
Rodrigo Henriques <rodrigo.henriques@m4u.com.br>
2016-01-09 21:01:54 -0200

    initial commit

---
+++ app/src/main/java/com/github/rodrigohenriques/picnic/ProductsActivity.java
@@ -0,0 +1,13 @@
+package com.github.rodrigohenriques.picnic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+    }
+}
